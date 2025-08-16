79d456e6bce9c0062b383ee20a9f9fcfd1220afa
Markus-Kivinen <markus.kivinen2@gmail.com>
2025-08-16 19:22:40 +0300

    11.11 Automatic deployments

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const app = express()
 require('dotenv').config()
 
 const PORT = process.env.PORT || 5000
+const VERSION = process.env.VERSION || '1.1.0'
 
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) => {
-  res.send(process.env.VERSION)
+  res.send(VERSION)
 })
 
 app.listen(PORT, () => {
